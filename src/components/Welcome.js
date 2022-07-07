ae9528f53d66639196c00a0660c7e4b634365b18
8eauty <106524498+8eauty@users.noreply.github.com>
2022-07-07 16:16:59 +0530

    Welcome.js

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -3,7 +3,10 @@ import React from 'react'
 
 function Welcome(props) {
   return (
-    <h1>"Hey!"{props.name}</h1>
+    <div>
+    <h1>Hey!{props.name}</h1>
+<h2>Welcome to Newton School.</h2>
+</div>
   )
 }
 
